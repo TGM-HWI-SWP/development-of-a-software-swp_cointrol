ec5386c94d0f003adefe333bf20f618bf07e82e5
istrainovic <ivan.strainovic@gmail.com>
2025-12-21 16:54:28 +0100

    Verbesserung: heplers.py

--- src/coin_trol/utils/helpers.py
+++ src/coin_trol/utils/helpers.py
@@ -1,6 +1,6 @@
 def find_user_by_name(name: str) -> int | None:
     """Sucht nach einem Benutzer und gibt seine ID zurück."""
-    from model.database import USERS
+    from coin_trol.model.database import USERS
     for user in USERS:
         if user.name.lower() == name.lower():
             return user.user_id
